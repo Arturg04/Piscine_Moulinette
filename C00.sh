4ab4ca8e43f03fe3eee08e443f97ffe0b23cad92
Artur <artur.13.goncalves@gmail.com>
2023-09-26 19:15:05 +0100

    up

--- C00.sh
+++ C00.sh
@@ -8,52 +8,64 @@ DEFAULT="\e[0m"
 src=~/tests/C00
 dest=temp/C00
 
-file=ex00/ft_putchar.c
-file2=temp/C00/ex00/ft_putchar.c
-file_main=temp/C00/ex00/main.c
-file_test=ex00/test.out
-file_final=temp/C00/ex00/final.out
-
 run_tests_for_C00()
 {
 	mkdir temp
 	cp -R "$src" "$dest"
+	# ex00
+	ex=ex00
+	file=ft_putchar.c
+	tester=ft_putchar.o
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
 
-	run_norminette "$file"
-	if [ $? -eq 0 ]; then
-		echo -e "${RED}----- ${file} -> FAIL ----- ${DEFAULT}"
-		return 0
-	fi
+	# ex01
+	ex=ex01
+	file=ft_print_alphabet.c
+	tester=ft_print_alphabet.c
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
 
-	compile "$file" "$file_main" "$file_test"
-	if [ $? -eq 0 ]; then
-		echo -n ""
-		#echo -e "${GREEN}Compilation successful${DEFAULT}"
-	else
-		echo -e "${RED}Compilation failed${DEFAULT}"
-		return 0
-	fi
-	compile "$file2" "$file_main" "$file_final"
+	# ex02
+	ex=ex02
+	file=ft_print_reverse_alphabet.c
+	tester=ft_print_reverse_alphabet.c
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
 
+	# ex03
+	ex=ex03
+	file=ft_print_numbers.c
+	tester=ft_print_numbers.c
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
 
-	# Run the final and test programs
-	./"$file_final" > "temp/output_final.txt"
-	./"$file_test" > "temp/output_test.txt"
+	# ex04
+	ex=ex04
+	file=ft_is_negative.c
+	tester=ft_is_negative.c
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
 
-	# Compare the output of the final and test programs
-	if diff "temp/output_final.txt" "temp/output_test.txt" &> /dev/null; then
-		echo -e "${GREEN}----- ${file} -> CORRECT ----- ${DEFAULT}"
-	else
-		echo -n "+ "
-		cat -e "temp/output_final.txt"
-		echo ""
-		echo -n "- "
-		cat -e "temp/output_test.txt"
-		echo ""
-		echo -e "${RED}----- ${file} -> FAIL ----- ${DEFAULT}"
-	fi
+	# ex05
+	ex=ex05
+	file=ft_print_comb.c
+	tester=ft_print_comb.c
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
 
-	rm -f "$file_test"
+	# ex06
+	ex=ex06
+	file=ft_print_comb2.c
+	tester=ft_print_comb2.c
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
+
+	# ex07
+	ex=ex07
+	file=ft_putnbr.c
+	tester=ft_putnbr.c
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
+
+	# ex08
+	ex=ex08
+	file=ft_print_combn.c
+	tester=ft_print_combn.c
+	test_ex "$ex"/"$file" "$dest"/"$ex"/"$tester" "$dest"/"$ex"/main.c
+
 	rm -r -f temp
 }
 
@@ -89,5 +101,50 @@ compile()
 	# Return the exit status to the calling code
 	return "$exit_status"
 }
+
+test_ex()
+{
+	local ex="$1"
+	local template="$2"
+	local main="$3"
+	local file_test=temp/C00/test.out
+	local file_final=temp/C00/final.out
+
+	run_norminette "$ex"
+	if [ $? -eq 0 ]; then
+		echo -e "${RED}----- ${ex00} -> FAIL ----- ${DEFAULT}"
+		return 0
+	fi
+
+	compile "$ex" "$main" "$file_test"
+	if [ $? -eq 0 ]; then
+		echo -n ""
+		#echo -e "${GREEN}Compilation successful${DEFAULT}"
+	else
+		echo -e "${RED}Compilation failed${DEFAULT}"
+		return 0
+	fi
+	compile "$template" "$main" "$file_final"
+
+	# Run the final and test programs
+	./"$file_final" > "temp/output_final.txt"
+	./"$file_test" > "temp/output_test.txt"
+
+	# Compare the output of the final and test programs
+	if diff "temp/output_final.txt" "temp/output_test.txt" &> /dev/null; then
+		echo -e "${GREEN}----- ${ex} -> CORRECT ----- ${DEFAULT}"
+	else
+		echo -n "+ "
+		cat -e "temp/output_final.txt"
+		echo ""
+		echo -n "- "
+		cat -e "temp/output_test.txt"
+		echo ""
+		echo -e "${RED}----- ${ex} -> FAIL ----- ${DEFAULT}"
+	fi
+
+	rm -f "$file_test"  "$file_final"
+}
+
 
 run_tests_for_C00
