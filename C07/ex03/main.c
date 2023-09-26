48053cf8201115a0c96dd08cd85c7074a5053b09
Artur <artur.13.goncalves@gmail.com>
2023-09-27 00:02:40 +0100

    up

--- C07/ex03/main.c
+++ C07/ex03/main.c
@@ -6,7 +6,7 @@ char	*ft_strjoin(int size, char **strs, char *sep);
 int main(void)
 {
 	char *strs1[] = {"Hello", "world!"};
-	char *strs2[] = {};
+	//char *strs2[] = {};
 	char *strs3[] = {NULL};
 	char *strs4[] = {"This", "is", "a", "test."};
 	char *strs5[] = {"This", "is", "a", "very", "long", "test."};
@@ -16,19 +16,19 @@ int main(void)
 	char *sep3 = ",";
 
 	char *res1 = ft_strjoin(2, strs1, sep1);
-	char *res2 = ft_strjoin(0, strs2, sep2);
+	//char *res2 = ft_strjoin(0, strs2, sep2);
 	char *res3 = ft_strjoin(1, strs3, sep3);
 	char *res4 = ft_strjoin(4, strs4, sep1);
 	char *res5 = ft_strjoin(6, strs5, sep3);
 
 	printf("res1: \"%s\"\n", res1);
-	printf("res2: \"%s\"\n", res2);
+	//printf("res2: \"%s\"\n", res2);
 	printf("res3: \"%s\"\n", res3);
 	printf("res4: \"%s\"\n", res4);
 	printf("res5: \"%s\"\n", res5);
 
 	free(res1);
-	free(res2);
+	//free(res2);
 	free(res3);
 	free(res4);
 	free(res5);
